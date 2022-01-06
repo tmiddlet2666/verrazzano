92be0663fa6345a1af43ba0ec6c8f1bc3a4daf91
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-01-06 17:56:39 +0530

    FIX SCRIPT PATH

--- tests/e2e/config/scripts/terraform/multicluster/delete-vcn.sh
+++ tests/e2e/config/scripts/terraform/multicluster/delete-vcn.sh
@@ -4,6 +4,7 @@
 # Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
 #
 
-. ./../cluster/init.sh
+CURR_SCRIPT_DIR=$(cd $(dirname "$0"); pwd -P)
+. $CURR_SCRIPT_DIR/../cluster/init.sh
 
 $SCRIPT_DIR/terraform destroy -auto-approve -no-color
