b88cbeb8a4e786811672cdc3e900f6aa7b7430c4
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-01-06 17:19:24 +0530

    add creating vcn

---
+++ tests/e2e/config/scripts/terraform/multicluster/create-vcn.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+#
+# Copyright (c) 2022, Oracle and/or its affiliates.
+# Licensed under the Universal Permissive License v 1.0 as shown at https://oss.oracle.com/licenses/upl.
+#
+
+. ./../cluster/init.sh
+$SCRIPT_DIR/terraform init -no-color
+$SCRIPT_DIR/terraform plan -no-color
+
+set -o pipefail
+
+# retry 3 times, 30 seconds apart
+tries=0
+MAX_TRIES=3
+while true; do
+   tries=$((tries+1))
+   echo "terraform apply iteration ${tries}"
+   $SCRIPT_DIR/terraform apply  -auto-approve -no-color && break
+   if [ "$tries" -ge "$MAX_TRIES" ];
+   then
+      echo "Terraform apply tries exceeded.  Cluster creation has failed!"
+      break
+   fi
+   echo "Deleting Cluster Terraform and applying again"
+   ./delete-vcn.sh
+   sleep 30
+done
+
+if [ "$tries" -ge "$MAX_TRIES" ];
+then
+  exit 1
+fi
