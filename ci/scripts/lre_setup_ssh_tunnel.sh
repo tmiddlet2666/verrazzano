da4e913756a1c21271eddf4768fdf9ab7b4046f4
BHAVANI.RAVICHANDRAN@ORACLE.COM <bhavani.ravichandran@oracle.com>
2022-01-10 17:17:16 +0000

    fix keycloak url

--- ci/scripts/lre_setup_ssh_tunnel.sh
+++ ci/scripts/lre_setup_ssh_tunnel.sh
@@ -55,7 +55,7 @@ if [ -z "$COMMAND" ]; then
     echo "didn't find the command to set up ssh tunnel"
     exit 1
 fi
-COMMAND+=" -o StrictHostKeyChecking=no -v -4 'ssh -L 6443:10.196.0.58:8443 keycloakadmin@mtRhg1ATZY1v6jJh' &"
+COMMAND+=" -o StrictHostKeyChecking=no -v -4 'ssh -L 6443:10.196.0.58:8443 keycloakadmin@keycloak.default.138.3.69.217.nip.io' &"
 echo "command = ${COMMAND}"
 echo "Setting up the ssh tunnel"
 eval ${COMMAND}
