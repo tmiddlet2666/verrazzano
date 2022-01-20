1edeb4b9f4b5ff0a6ae3806113bc8081dd6fdc21
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-01-20 12:12:57 +0530

    lb port forward

--- tests/e2e/config/scripts/terraform/cluster/create-pfsession-lb.sh
+++ tests/e2e/config/scripts/terraform/cluster/create-pfsession-lb.sh
@@ -1,37 +1,18 @@
 #!/bin/bash
 
 compartment_id=$1
-region=$2
-cluster_name=$3
-bastion_name=$4
-public_key_file=$5
-private_key_file=$6
-KUBECONFIG=$7
-port=$8
+bastion_name=$2
+KUBECONFIG=$3
+public_key_file=$4
+private_key_file=$5
+port=$6
+target_subnet_id=$7
 echo "CREATE KUBECONFIG at $KUBECONFIG"
 
-#oci bastion bastion create --bastion-type STANDARD --compartment-id $compartment_id --target-subnet-id $target_subnet_id --client-cidr-list '["0.0.0.0/0"]' --max-session-ttl 10800 --name $bastion_name
-#exit 0
-
-rm $KUBECONFIG
-#oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaarkzyy7cfaxopanhis2lwacgxbiu3x3cctxzna3fbgcteytlth3gq
-oci ce cluster list -c $compartment_id
-#exit 0
-
-cluster_id=$(oci ce cluster list -c $compartment_id --name $cluster_name --lifecycle-state ACTIVE | jq '.data[].id' | sed -e 's/^"//' -e 's/"$//')
-oci ce cluster create-kubeconfig \
-	--cluster-id $cluster_id \
-	--file $KUBECONFIG \
-	--region $region \
-	--token-version 2.0.0 \
-	--kube-endpoint PRIVATE_ENDPOINT
 
 bastion_id=$(oci bastion bastion list -c $compartment_id --name $bastion_name --bastion-lifecycle-state ACTIVE --all | jq '.data[].id' | sed -e 's/^"//' -e 's/"$//')
-api_private_endpoint=$(oci ce cluster get --cluster-id $cluster_id | jq '.data.endpoints["private-endpoint"]' | sed -e 's/^"//' -e 's/"$//')
-private_ip=$(echo "$api_private_endpoint" | cut -d ':' -f1)
-session_id=$(oci bastion session create-port-forwarding --bastion-id $bastion_id --target-private-ip $private_ip --session-ttl 10800 --target-port 6443 --ssh-public-key-file $public_key_file --wait-for-state SUCCEEDED | jq -r '.data.resources[].identifier')
-
-echo "ACCESS KUBERNETES CLUSTER VIA PORT FORWARDING"
+private_ip=$(kubectl --kubeconfig $KUBECONFIG get vz -o yaml | yq e ".items[0].status.instance.rancherUrl" - | cut -d "." -f3- | cut -d "n" -f1 | rev | cut -c2- | rev)
+session_id=$(oci bastion session create-port-forwarding --bastion-id $bastion_id --target-private-ip $private_ip --session-ttl 10800 --target-port 443 --ssh-public-key-file $public_key_file --wait-for-state SUCCEEDED | jq -r '.data.resources[].identifier')
 username=$(oci bastion session get --session-id $session_id | jq '.data["target-resource-details"]["target-resource-operating-system-user-name"]' | sed -e 's/^"//' -e 's/"$//')
 bastion_ip=$(oci bastion session get --session-id $session_id | jq '.data["target-resource-details"]["target-resource-private-ip-address"]' | sed -e 's/^"//' -e 's/"$//')
 tunnel_command=$(oci bastion session get --session-id $session_id | jq '.data["ssh-metadata"]["command"]' | sed -e 's/^"//' -e 's/"$//')
@@ -53,16 +34,16 @@ tunnel_command="${tunnel_command//ssh -i/ssh -4 -v -o StrictHostKeyChecking=no -
 
 tunnel_command="while true; do { while true; do echo echo ping; sleep 10; done } | ${tunnel_command};sleep 10;done &"
 
-cp $KUBECONFIG "${KUBECONFIG}_original"
-
-# Substitute 127.0.0.1 into kubeconfig file
-sed -i.bak "s/${api_private_endpoint}/127.0.0.1:$port/g" $KUBECONFIG
-
 echo $tunnel_command
 
 # Run SSH command
 eval $tunnel_command
 
-sleep 5
+list=$(kubectl --kubeconfig $KUBECONFIG get vz -o yaml | yq e ".items[0].status.instance" -)
+while IFS= read -r line; do
+    echo "$private_ip $(echo $line | cut -d "/" -f3-)" >> /etc/hosts
+done <<< "$list"
+cat /etc/hosts
+sudo iptables -t nat -A OUTPUT -p tcp --dport 443 -d $private_ip -j DNAT --to-destination 127.0.0.1:$port
 
-echo "KUBECTL READY TO USE"
+sleep 5
