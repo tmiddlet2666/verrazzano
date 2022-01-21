68a28d7708086e9237b7d5b9671f0a2879c495f2
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-01-21 11:24:37 +0530

    test

--- tests/e2e/config/scripts/terraform/cluster/create-pfsession-lb.sh
+++ tests/e2e/config/scripts/terraform/cluster/create-pfsession-lb.sh
@@ -11,7 +11,7 @@ echo "CREATE KUBECONFIG at $KUBECONFIG"
 
 kubectl --kubeconfig $KUBECONFIG get vz -o yaml
 bastion_id=$(oci bastion bastion list -c $compartment_id --name $bastion_name --bastion-lifecycle-state ACTIVE --all | jq '.data[].id' | sed -e 's/^"//' -e 's/"$//')
-private_ip=$(kubectl --kubeconfig $KUBECONFIG get vz -o yaml | yq e ".items[0].status.instance.elasticUrl" - | cut -d "." -f5- | cut -d "n" -f1 | rev | cut -c2- | rev)
+private_ip=$(kubectl --kubeconfig $KUBECONFIG get vz -o yaml | yq e ".items[0].status.instance.prometheusUrl" - | cut -d "." -f5- | cut -d "n" -f1 | rev | cut -c2- | rev)
 session_id=$(oci bastion session create-port-forwarding --bastion-id $bastion_id --target-private-ip $private_ip --session-ttl 10800 --target-port 443 --ssh-public-key-file $public_key_file --wait-for-state SUCCEEDED | jq -r '.data.resources[].identifier')
 username=$(oci bastion session get --session-id $session_id | jq '.data["target-resource-details"]["target-resource-operating-system-user-name"]' | sed -e 's/^"//' -e 's/"$//')
 bastion_ip=$(oci bastion session get --session-id $session_id | jq '.data["target-resource-details"]["target-resource-private-ip-address"]' | sed -e 's/^"//' -e 's/"$//')
@@ -39,11 +39,15 @@ echo $tunnel_command
 # Run SSH command
 eval $tunnel_command
 
+touch /tmp/hosts
 list=$(kubectl --kubeconfig $KUBECONFIG get vz -o yaml | yq e ".items[0].status.instance" -)
 while IFS= read -r line; do
-    sudo echo "$private_ip $(echo $line | cut -d "/" -f3-)" >> /etc/hosts
+    echo "$private_ip $(echo $line | cut -d "/" -f3-)" >> /tmp/hosts
 done <<< "$list"
+cat /tmp/hosts
+sudo cat /tmp/hosts >> /etc/hosts
 sudo cat /etc/hosts
+rm -rf /tmp/hosts
 sudo iptables -t nat -A OUTPUT -p tcp --dport 443 -d $private_ip -j DNAT --to-destination 127.0.0.1:$port
 
 sleep 5
