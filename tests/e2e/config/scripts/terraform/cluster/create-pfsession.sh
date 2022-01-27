65730cc7904021f64fdabdf23376a972671d6a79
Ravi Singhal <ravi.r.singhal@oracle.com>
2022-01-27 16:24:34 +0530

    cleanup lb

--- tests/e2e/config/scripts/terraform/cluster/create-pfsession.sh
+++ tests/e2e/config/scripts/terraform/cluster/create-pfsession.sh
@@ -19,24 +19,19 @@ port=$8
 #exit 0
 
 rm $KUBECONFIG
-oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaag22w3hm6dlk2sfrx5ubxwuxsgw5a2cis6ipor53dzcqugc7fhbaa
-oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaah2t2q5ht4jfvizwkwrxo7ygpap52bdcsa2id3j4mlcseuhtzb5hq
-oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaaufnshh7bcnmekjv3h236wmq7no6zfvkpyjiifyetqcwgd3l6bu6q
-oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaajrvel64ujiruzjz7vp5phk6iboxrmit2l6rknio66c57zbnjoiua
+#oci ce cluster delete --force --cluster-id=ocid1.cluster.oc1.ap-tokyo-1.aaaaaaaag22w3hm6dlk2sfrx5ubxwuxsgw5a2cis6ipor53dzcqugc7fhbaa
 
 
-
-
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaaari2opwkhfvgvwxph6bnyras7p7y6hbp66zsmmiixej3vstnnggq
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaafu25usyjekxx35rwpd3cwzofzhfha2rbl3je3rvzws3izoimif5q
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaagd73skq7bq4qoqsvtfld2feikbjmiigesuyr64vau5rxwoxzt5dq
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaagkvlsf4wzmayz5udech44ecdwukrygecuwdnvdvoyrm35ykt233q
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaagphricwl24fxv5zaip2tuejqcxwniizekifqlw5ntcgnqrhrueta
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaagwttxqt3gjuckpqe4vnat4vj6lwphcen2qolvme2ddlbtxnhepiq
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaaijxtqgx6dfcvdqpbydncviz5rsdwhegolbcfs5th2ghgnrqpleoa
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaaimyykxoch3sm7qcxh4g3dzkdwyzq5erlirrcfg6rpgxzx7dpgawa
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaaivjmwi6x6hffasltgju7vzo3xlt4dpwf36j3pwuoplxqqkg7qwxq
-oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaajhkmdkkdf2rmbxtt2ogtjdrrd3f3hada2mcsp2ms6x3sjw66phqq
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaa3oj732nwy5fvg5xbumnnf7snd7ogof6l6t6e46kltcsf64krav4q
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaaf7ra6m6ik4atersyplmfrg7posn6valet7ozbl5sfxamujhihqxa
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaajjmdo3efgfrufvgxrgrurllyw52qhofa2alsxw2vue3p3eobkrbq
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaajvpyz4egliy6ppoigrtgx3fjylpyitufnik3wqoa75xfs72vqv2a
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaakxiliv3hnbedn64mrcjt7rfsm266tljjfcivm7777ygeoi62xnha
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaam4er7hnada4mgane255drgue3turk25z6yigwmysntnkl5mje66q
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaan77rcwzlmr3fjmlbtlrk7ovoeq2n7yrueflrttzfujiwjn7khpka
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaan7aovddxclloaqkwlsavpzza3hlpxlp6gnzd7p7nr2ww5wv2pamq
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaanc5u3dwnw3nda4czenq5csscbdsvycqiebswyfyfnrq6npmxwtua
+oci lb load-balancer delete --force --load-balancer-id=ocid1.loadbalancer.oc1.ap-tokyo-1.aaaaaaaanxnzpuhmb5kv7mvhkvo7zostqeoql25ntvysfequ5kvrsj5bhwza
 oci ce cluster list -c $compartment_id
 #exit 0
 oci lb load-balancer list --compartment-id=$compartment_id
